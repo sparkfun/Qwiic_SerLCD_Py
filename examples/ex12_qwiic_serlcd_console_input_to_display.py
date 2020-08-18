9c62d102cb5aac12e05efb4cdd8fcd65352e850b
Pete Lewis <pete.lewis@sparkfun.local>
2020-08-18 08:07:24 -0600

    ex12 working

---
+++ examples/ex12_qwiic_serlcd_console_input_to_display.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+#-----------------------------------------------------------------------------
+# ex12_qwiic_serlcd_console_input_to_display.py
+#
+# This example demonstrates how to take text input from the python console
+# and send it to the LCD.
+#
+#------------------------------------------------------------------------
+#
+# Written by SparkFun Electronics, August 2020
+#
+# Ported from Arduino Library code with many contributions from
+# Gaston Williams - August 29, 2018
+# 
+# This python library supports the SparkFun Electroncis qwiic 
+# qwiic sensor/board ecosystem on a Raspberry Pi (and compatable) single
+# board computers. 
+#
+# More information on qwiic is at https://www.sparkfun.com/qwiic
+#
+# Do you like this library? Help support SparkFun. Buy a board!
+#
+#==================================================================================
+# Copyright (c) 2020 SparkFun Electronics
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy 
+# of this software and associated documentation files (the "Software"), to deal 
+# in the Software without restriction, including without limitation the rights 
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+# copies of the Software, and to permit persons to whom the Software is 
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+#==================================================================================
+# Example 12
+#
+
+from __future__ import print_function
+import qwiic_serlcd
+import time
+import sys
+
+def runExample():
+
+	print("\nSparkFun Qwiic SerLCD   Example 12\n")
+	print("\nType CTRL+C to end.\n")
+	myLCD = qwiic_serlcd.QwiicSerlcd()
+
+	if myLCD.connected == False:
+		print("The Qwiic SerLCD device isn't connected to the system. Please check your connection", \
+			file=sys.stderr)
+		return
+
+	myLCD.setBacklight(255, 255, 255) # Set backlight to bright white
+	myLCD.setContrast(5) # set contrast. Lower to 0 for higher contrast.
+	myLCD.begin() # call this for default settings
+	myLCD.leftToRight()
+	myLCD.noCursor()
+	time.sleep(1) # give a sec for system messages to complete
+
+	while True:
+		
+		# promt the user to input some text
+		user_input = input("Please type something to display on the LCD: ")
+		
+		myLCD.clearScreen() # clear the screen
+
+		myLCD.print(user_input) # print what the user just typed in
+		
+		time.sleep(0.5) # wait a second
+
+if __name__ == '__main__':
+	try:
+		runExample()
+	except (KeyboardInterrupt, SystemExit) as exErr:
+		print("\nEnding Example 12")
+		sys.exit(0)
+
+
